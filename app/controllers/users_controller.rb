47b2ced8e4f84f483ff266f5abf2bb95c8ae21e2
mrwerner392 <mrwerner392@gmail.com>
2019-10-30 15:51:31 -0400

    can create user and receive token and id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
 
   def create
     user = User.create(user_params)
@@ -7,6 +8,12 @@ class UsersController < ApplicationController
     else
       render json: {errors: user.errors.full_messages }, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def user_params
+    params.permit(:first_name, :username, :password, :age, :gender, :bio, :college, :occupation)
   end
 
 end
