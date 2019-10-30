bce3efb73739c24146bc8135c23fa5b5ca341449
mrwerner392 <mrwerner392@gmail.com>
2019-10-30 15:57:24 -0400

    topics resources and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_30_183704) do
+ActiveRecord::Schema.define(version: 2019_10_30_195635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "topics", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
