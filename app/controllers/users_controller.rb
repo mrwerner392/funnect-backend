4e525292eebfaac0c21cec97bbe130de350dc1eb
mrwerner392 <mrwerner392@gmail.com>
2019-10-30 15:30:57 -0400

    users controller create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+
+  def create
+    user = User.create(user_params)
+    if user.valid?
+      render json: authentication_json(user.id)
+    else
+      render json: {errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
 end
