5691a96fc0b598dac33f2f0afcc4aa1e914de420
mrwerner392 <mrwerner392@gmail.com>
2019-10-31 21:10:19 -0400

    can create user_interests on create user action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,12 @@ class UsersController < ApplicationController
   def create
     user = User.create(user_params)
     if user.valid?
+      byebug
+      interests = params[:interests]
+      interests.each do |interest|
+        UserInterest.create(user: user, interest: Interest.find_by(name: interest))
+      end
+      byebug
       render json: authentication_json(user.id)
     else
       render json: {errors: user.errors.full_messages }, status: :unprocessable_entity
