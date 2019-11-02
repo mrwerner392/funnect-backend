8f4a3fde8cb650e7c577517967571715d00326dd
mrwerner392 <mrwerner392@gmail.com>
2019-11-01 20:39:22 -0400

    removed unnecessary files

--- app/serializers/created_posts_serializer.rb
+++
@@ -1,9 +0,0 @@
-class CreatedPostsSerializer < ActiveModel::Serializer
-  attributes :id, :date, :time_of_day, :description
-  
-  has_many :interested_users
-  has_one :user
-  has_one :topic
-  has_one :neighborhood
-
-end
--- app/serializers/post_interest_serializer.rb
+++
@@ -1,5 +0,0 @@
-class PostInterestSerializer < ActiveModel::Serializer
-  attributes :id
-  has_one :user
-  has_one :post
-end
