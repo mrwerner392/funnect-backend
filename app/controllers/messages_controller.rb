08500d9ddd3c5ea74eef0165a0c924f5dcedfa1d
mrwerner392 <mrwerner392@gmail.com>
2019-11-03 16:51:37 -0500

    channel unique to event

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,13 @@
 class MessagesController < ApplicationController
+
+  def create
+    event = Event.find(params[:event_id])
+    message = Message.Create(user_id: params[:user_id], event: event, content: params[:content])
+    if message.valid?
+      ActionCable.server.broadcast_to(event, {type: 'NEW_MESSAGE', payload: message)
+      render json: message
+    else
+      render json: {errors: message.errors.full_messages}, status: 422
+  end
+
 end
