357b9f23d2b89432454e19195a8198069df78a75
mrwerner392 <mrwerner392@gmail.com>
2019-11-02 21:09:06 -0400

    typo fix create post action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,8 +6,9 @@ class PostsController < ApplicationController
   end
 
   def create
-    date = params[:day] == 'Today' ? Data.today : Date.tomorrow
-    post = Post.create(user_id: params[:id], topic_id: params[:topic], neighborhood_id: params[:neighborhood], description: params[:description], date: data, time_of_day: params[:time_of_day])
+    byebug
+    date = params[:day] == 'Today' ? Date.today : Date.tomorrow
+    post = Post.create(user_id: params[:id], topic_id: params[:topic], neighborhood_id: params[:neighborhood], description: params[:description], date: date, time_of_day: params[:time_of_day])
     if post.valid?
       render json: post
     else
