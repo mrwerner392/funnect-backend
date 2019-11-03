6a26ed3cc1e884907db69adb8aa8c9bf30684f83
mrwerner392 <mrwerner392@gmail.com>
2019-11-02 22:11:23 -0400

    groundwork for event create action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,13 @@
 class EventsController < ApplicationController
+
+  def create
+    post = Post.find(params[:post_id])
+    event = Event.create(user_id: params[:user_id], post: post, description: post.description, date: post.date)
+    if event.valid?
+      params[:attendees].each do |attendee|
+        EventAttendee.create(event: event, user: attendee)
+      end
+    render json: event
+  end
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,6 @@ class PostsController < ApplicationController
   end
 
   def create
-    byebug
     date = params[:day] == 'Today' ? Date.today : Date.tomorrow
     post = Post.create(user_id: params[:id], topic_id: params[:topic], neighborhood_id: params[:neighborhood], description: params[:description], date: date, time_of_day: params[:time_of_day])
     if post.valid?
