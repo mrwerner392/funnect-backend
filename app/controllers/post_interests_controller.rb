f391334e074dd03a450568187f218c7541c95b7c
mrwerner392 <mrwerner392@gmail.com>
2019-11-05 08:34:13 -0500

    post interest create and destroy actions

--- app/controllers/post_interests_controller.rb
+++ app/controllers/post_interests_controller.rb
@@ -1,2 +1,19 @@
 class PostInterestsController < ApplicationController
+
+  def create
+    post = Post.find(params[:post_id])
+    post_interest = PostInterest.create(post: post, user_id: params[:user_id])
+    if post_interest.valid?
+      render json: post
+    else
+      render json: {errors: post_interest.errors.full_messages}
+    end
+  end
+
+  def destroy
+    post_interest = PostInterest.find(params[:id])
+    post_interest.destroy
+    render json: {}
+  end
+
 end
