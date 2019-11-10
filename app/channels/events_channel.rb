91d807e5d05ea8d1dd79c5515da36cf6b58a21e6
mrwerner392 <mrwerner392@gmail.com>
2019-11-09 22:33:04 -0500

    events channel setup

--- app/channels/events_channel.rb
+++ app/channels/events_channel.rb
@@ -1,9 +1,9 @@
 class EventsChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "events_channel"
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+    raise NotImplementedError
   end
 end
