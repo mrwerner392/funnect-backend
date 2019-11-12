fc12e2a8fd8fc30278374f075a9fd645d4544589
mrwerner392 <mrwerner392@gmail.com>
2019-11-11 20:58:20 -0500

    message for broadcast

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,17 @@ class Message < ApplicationRecord
 
   belongs_to :user
   belongs_to :event
-  
+
+  def sender
+    self.user.first_name
+  end
+
+  def message_for_broadcast
+    {
+      id: self.id,
+      content: self.content,
+      sender: self.sender
+    }
+  end
+
 end
--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,4 +1,3 @@
 class MessageSerializer < ActiveModel::Serializer
-  attributes :id, :content
-  has_one :user
+  attributes :id, :content, :sender
 end
